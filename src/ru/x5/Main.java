f130c45ac546b562bb86ba5a2f0770afce8c788f
Kir.Kudryavtsev <Dragon348@mail.ru>
2020-09-01 13:03:39 +0300

    Решена Задача 7

--- src/ru/x5/Main.java
+++ src/ru/x5/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Reader reader = new Reader();
-        Task1 task1 = new Task1();
-        Task2 task2 = new Task2();
-        Task3 task3 = new Task3();
-        Task4 task4 = new Task4();
+        Task7 task = new Task7();
+
         //System.out.println("Задача 1:");
-        task4.solveTask(reader);
+        task.solveTask(reader);
     }
 }
--- src/ru/x5/Task1.java
+++ src/ru/x5/Task1.java
@@ -1,6 +1,8 @@
 package ru.x5;
-/*Ввести целое число в консоли и вывести на экран
-последнюю цифру введенного числа.*/
+/*
+Ввести целое число в консоли и вывести на экран
+последнюю цифру введенного числа.
+*/
 public class Task1 {
     public void printLastDigit(int number){
         System.out.println(number % 10);
