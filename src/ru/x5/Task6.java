0a714ec295ff195b72427f3e99935a618e0af830
Kir.Kudryavtsev <Dragon348@mail.ru>
2020-09-01 12:43:34 +0300

    Решена Задача 6

---
+++ src/ru/x5/Task6.java
@@ -0,0 +1,21 @@
+package ru.x5;
+/*Ввести целое число в консоли. Вывести его строку-
+описание вида «отрицательное четное число»,
+«нулевое число», «положительное нечетное число» и
+т. д.*/
+public class Task6 {
+    public void printDescription(int number){
+        if (number == 0) System.out.println("нулевое число");
+        else {
+            if (number > 0) System.out.print("положительное ");
+            else System.out.print("отрицательное ");
+            if (number % 2 == 0) System.out.print("четное ");
+            else System.out.print("нечетное ");
+            System.out.println("число");
+        }
+    }
+    public void solveTask(Reader reader){
+        System.out.print("Введите целое число: ");
+        printDescription(reader.readInt());
+    }
+}
